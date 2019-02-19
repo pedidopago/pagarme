6c085ac26f9c67c0a27324c05b77da5362da3ffa
Gabriel Ochsenhofer <gabs@gabs.tv>
2019-02-19 19:52:38 -0300

    billet test

--- pkg/pagarme/transactions/transactions_test.go
+++ pkg/pagarme/transactions/transactions_test.go
@@ -241,3 +241,74 @@ func TestRefundCard(t *testing.T) {
 		t.Fatal(z1.Status, "!=", pagarme.TrPaid, z1)
 	}
 }
+
+func TestPutBoleto(t *testing.T) {
+	adr0 := &pagarme.Address{
+		Zipcode:       "01232000",
+		Street:        "Alameda Barros",
+		StreetNumber:  "400",
+		Complementary: "Apto 1000",
+		State:         "SP",
+		City:          "Sao Paulo",
+		Country:       "br",
+	}
+	tr0 := &pagarme.Transaction{
+		Amount:               15669, // R$ 156.69
+		CardHolderName:       "John Smith",
+		PaymentMethod:        pagarme.PaymentBoleto,
+		BoletoExpirationDate: time.Now().AddDate(0, 0, 2).Format("2006-01-02"),
+		BoletoInstructions:   "test billet please ignore",
+		Async:                false,
+		Installments:         1,
+		SoftDescriptor:       "teste boleto",
+		Capture:              "true",
+		Customer: &pagarme.Customer{
+			ExternalID: "TESTEJOHN1",
+			Name:       "John Smith",
+			Country:    pagarme.Brazil,
+			Type:       pagarme.CustomerIndividual,
+			Email:      "john.smith@gmail.com",
+			PhoneNumbers: []string{
+				"+5511900000000",
+			},
+			Documents: []*pagarme.Document{
+				&pagarme.Document{
+					Type:   pagarme.DocCPF,
+					Number: "82700962028",
+				},
+			},
+		},
+		Billing: &pagarme.Billing{
+			Name:    "John Smith",
+			Address: adr0,
+		},
+		Shipping: &pagarme.Shipping{
+			Name:         "John Smith",
+			Address:      adr0,
+			DeliveryDate: pagarme.YYYYMMDDFromTime(time.Now().AddDate(0, 0, 2)),
+		},
+		Items: []*pagarme.Item{
+			&pagarme.Item{
+				ID:        "test_1",
+				Title:     "Vitamina C 100mg (100 cap)",
+				UnitPrice: 4568,
+				Tangible:  true,
+				Quantity:  1,
+			},
+		},
+	}
+	t9 := www.JSONReader(tr0)
+	gg, _ := ioutil.ReadAll(t9)
+	t.Log(string(gg))
+	cfg := pagarme.Default(getKeysEnv())
+	//cfg.Trace = true
+	trs := New(cfg)
+	z0, z1, err := trs.Put(tr0)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if z0.Code != pagarme.ResponseCodeOk {
+		t.Fatal(z0.String())
+	}
+	t.Log(z1)
+}
