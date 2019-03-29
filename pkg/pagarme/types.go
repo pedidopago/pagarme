b6863449a50e123279f0fdbe1c3be7ebee8eae33
gabstv <gabs@Gabriels-Mac-mini.local>
2019-03-29 12:58:29 -0300

    form tags

--- pkg/pagarme/types.go
+++ pkg/pagarme/types.go
@@ -6,104 +6,109 @@ import (
 	"time"
 )
 
+// Address TBD
 type Address struct {
-	Object       string `json:"object,omitempty"`
-	ID           int64  `json:"id,omitempty"`
-	Street       string `json:"street"`
-	StreetNumber string `json:"street_number"`
+	Object       string `json:"object,omitempty" form:"object,omitempty"`
+	ID           int64  `json:"id,omitempty" form:"id,omitempty"`
+	Street       string `json:"street" form:"street"`
+	StreetNumber string `json:"street_number" form:"street_number"`
 	// CEP. Para endereço brasileiro, deve conter uma numeração de 8 dígitos
-	Zipcode string `json:"zipcode"`
+	Zipcode string `json:"zipcode" form:"zipcode"`
 	// Obrigatório. País. Duas letras minúsculas. Deve seguir o padão ISO 3166-1 alpha-2
-	Country       CountryCode `json:"country"`
-	State         string      `json:"state"`
-	City          string      `json:"city"`
-	Neighborhood  string      `json:"neighborhood,omitempty"`
-	Complementary string      `json:"complementary,omitempty"`
+	Country       CountryCode `json:"country" form:"country"`
+	State         string      `json:"state" form:"state"`
+	City          string      `json:"city" form:"city"`
+	Neighborhood  string      `json:"neighborhood,omitempty" form:"neighborhood,omitempty"`
+	Complementary string      `json:"complementary,omitempty" form:"complementary,omitempty"`
 }
 
+// Document TBD
 type Document struct {
-	Object string       `json:"object,omitempty"`
-	ID     string       `json:"id,omitempty"`
-	Type   DocumentType `json:"type,omitempty"`
-	Number string       `json:"number,omitempty"`
+	Object string       `json:"object,omitempty" form:"object,omitempty"`
+	ID     string       `json:"id,omitempty" form:"id,omitempty"`
+	Type   DocumentType `json:"type,omitempty" form:"type,omitempty"`
+	Number string       `json:"number,omitempty" form:"number,omitempty"`
 }
 
+// SplitRule TBD
 type SplitRule struct {
-	Object              string        `json:"object,omitempty"`
-	ID                  string        `json:"id,omitempty"`
-	Liable              bool          `json:"liable"`
-	ChargeProcessingFee bool          `json:"charge_processing_fee"`
-	Percentage          Float64String `json:"percentage,omitempty"`
-	Amount              Float64String `json:"amount,omitempty"`
-	ChargeRemainderFee  bool          `json:"charge_remainder_fee"`
-	RecipientID         string        `json:"recipient_id"`
+	Object              string        `json:"object,omitempty" form:"object,omitempty"`
+	ID                  string        `json:"id,omitempty" form:"id,omitempty"`
+	Liable              bool          `json:"liable" form:"liable"`
+	ChargeProcessingFee bool          `json:"charge_processing_fee" form:"charge_processing_fee"`
+	Percentage          Float64String `json:"percentage,omitempty" form:"percentage,omitempty"`
+	Amount              Float64String `json:"amount,omitempty" form:"amount,omitempty"`
+	ChargeRemainderFee  bool          `json:"charge_remainder_fee" form:"charge_remainder_fee"`
+	RecipientID         string        `json:"recipient_id" form:"recipient_id"`
 }
 
+// Customer TODO: godoc
 type Customer struct {
-	Object       string       `json:"object,omitempty"`
-	ID           int64        `json:"id,omitempty"`
-	ExternalID   string       `json:"external_id,omitempty"`
-	Type         CustomerType `json:"type"`
-	Name         string       `json:"name,omitempty"`
-	Email        string       `json:"email,omitempty"`
-	Country      CountryCode  `json:"country,omitempty"`
-	Birthday     YYYYMMDD     `json:"birthday,omitempty"`
-	Gender       string       `json:"gender,omitempty"`
-	Documents    []*Document  `json:"documents,omitempty"`
-	PhoneNumbers []string     `json:"phone_numbers,omitempty"`
+	Object       string       `json:"object,omitempty" form:"object,omitempty"`
+	ID           int64        `json:"id,omitempty" form:"id,omitempty"`
+	ExternalID   string       `json:"external_id,omitempty" form:"external_id,omitempty"`
+	Type         CustomerType `json:"type" form:"type"`
+	Name         string       `json:"name,omitempty" form:"name,omitempty"`
+	Email        string       `json:"email,omitempty" form:"email,omitempty"`
+	Country      CountryCode  `json:"country,omitempty" form:"country,omitempty"`
+	Birthday     YYYYMMDD     `json:"birthday,omitempty" form:"birthday,omitempty"`
+	Gender       string       `json:"gender,omitempty" form:"gender,omitempty"`
+	Documents    []*Document  `json:"documents,omitempty" form:"documents,omitempty"`
+	PhoneNumbers []string     `json:"phone_numbers,omitempty" form:"phone_numbers,omitempty"`
 }
 
+// Item TODO: godoc
 type Item struct {
-	ID        string `json:"id,omitempty"`
-	Title     string `json:"title,omitempty"`
-	UnitPrice int    `json:"unit_price,omitempty"`
-	Quantity  int    `json:"quantity,omitempty"`
-	Tangible  bool   `json:"tangible,omitempty"`
+	ID        string `json:"id,omitempty" form:"id,omitempty"`
+	Title     string `json:"title,omitempty" form:"title,omitempty"`
+	UnitPrice int    `json:"unit_price,omitempty" form:"unit_price,omitempty"`
+	Quantity  int    `json:"quantity,omitempty" form:"quantity,omitempty"`
+	Tangible  bool   `json:"tangible,omitempty" form:"tangible,omitempty"`
 }
 
 type Billing struct {
-	Address *Address `json:"address,omitempty"`
-	Object  string   `json:"object,omitempty"`
-	ID      int64    `json:"id,omitempty"`
-	Name    string   `json:"name,omitempty"`
+	Address *Address `json:"address,omitempty" form:"address,omitempty"`
+	Object  string   `json:"object,omitempty" form:"object,omitempty"`
+	ID      int64    `json:"id,omitempty" form:"id,omitempty"`
+	Name    string   `json:"name,omitempty" form:"name,omitempty"`
 }
 
 type Shipping struct {
-	Address      *Address `json:"address,omitempty"`
-	Object       string   `json:"object,omitempty"`
-	ID           int64    `json:"id,omitempty"`
-	Name         string   `json:"name,omitempty"`
-	Fee          int      `json:"fee"`
-	DeliveryDate YYYYMMDD `json:"delivery_date,omitempty"`
-	Expedited    bool     `json:"expedited,omitempty"`
+	Address      *Address `json:"address,omitempty" form:"address,omitempty"`
+	Object       string   `json:"object,omitempty" form:"object,omitempty"`
+	ID           int64    `json:"id,omitempty" form:"id,omitempty"`
+	Name         string   `json:"name,omitempty" form:"name,omitempty"`
+	Fee          int      `json:"fee" form:"fee"`
+	DeliveryDate YYYYMMDD `json:"delivery_date,omitempty" form:"delivery_date,omitempty"`
+	Expedited    bool     `json:"expedited,omitempty" form:"expedited,omitempty"`
 }
 
 type Card struct {
-	Object         string    `json:"object,omitempty"`
-	ID             string    `json:"id,omitempty"`
-	DateCreated    time.Time `json:"date_created,omitempty"`
-	DateUpdated    time.Time `json:"date_updated,omitempty"`
-	Brand          string    `json:"brand,omitempty"`
-	HolderName     string    `json:"holder_name,omitempty"`
-	FirstDigits    string    `json:"first_digits,omitempty"`
-	LastDigits     string    `json:"last_digits,omitempty"`
-	Country        string    `json:"country,omitempty"`
-	Fingerprint    string    `json:"fingerprint,omitempty"`
-	Valid          bool      `json:"valid,omitempty"`
-	ExpirationDate MMYY      `json:"expiration_date,omitempty"`
+	Object         string    `json:"object,omitempty" form:"object,omitempty"`
+	ID             string    `json:"id,omitempty" form:"id,omitempty"`
+	DateCreated    time.Time `json:"date_created,omitempty" form:"date_created,omitempty"`
+	DateUpdated    time.Time `json:"date_updated,omitempty" form:"date_updated,omitempty"`
+	Brand          string    `json:"brand,omitempty" form:"brand,omitempty"`
+	HolderName     string    `json:"holder_name,omitempty" form:"holder_name,omitempty"`
+	FirstDigits    string    `json:"first_digits,omitempty" form:"first_digits,omitempty"`
+	LastDigits     string    `json:"last_digits,omitempty" form:"last_digits,omitempty"`
+	Country        string    `json:"country,omitempty" form:"country,omitempty"`
+	Fingerprint    string    `json:"fingerprint,omitempty" form:"fingerprint,omitempty"`
+	Valid          bool      `json:"valid,omitempty" form:"valid,omitempty"`
+	ExpirationDate MMYY      `json:"expiration_date,omitempty" form:"expiration_date,omitempty"`
 }
 
 type Response struct {
-	Code   ResponseCode `json:"code,omitempty"`
-	Errors []Perror     `json:"errors,omitempty"`
-	URL    string       `json:"url,omitempty"`
-	Method string       `json:"method,omitempty"`
+	Code   ResponseCode `json:"code,omitempty" form:"code,omitempty"`
+	Errors []Perror     `json:"errors,omitempty" form:"errors,omitempty"`
+	URL    string       `json:"url,omitempty" form:"url,omitempty"`
+	Method string       `json:"method,omitempty" form:"method,omitempty"`
 }
 
 type Perror struct {
-	Message       string `json:"message,omitempty"`
-	ParameterName string `json:"parameter_name,omitempty"`
-	Type          string `json:"type,omitempty"`
+	Message       string `json:"message,omitempty" form:"message,omitempty"`
+	ParameterName string `json:"parameter_name,omitempty" form:"parameter_name,omitempty"`
+	Type          string `json:"type,omitempty" form:"type,omitempty"`
 }
 
 func (r *Response) String() string {
--- pkg/pagarme/types_transaction.go
+++ pkg/pagarme/types_transaction.go
@@ -5,71 +5,71 @@ import (
 )
 
 type Transaction struct {
-	Amount               int                    `json:"amount"`
-	CardHash             string                 `json:"card_hash,omitempty"`
-	CardID               string                 `json:"card_id,omitempty"`
-	CardHolderName       string                 `json:"card_holder_name,omitempty"`
-	CardExpirationDate   MMYY                   `json:"card_expiration_date,omitempty"`
-	CardNumber           string                 `json:"card_number,omitempty"`
-	CardCVV              string                 `json:"card_cvv,omitempty"`
-	PaymentMethod        PaymentMethod          `json:"payment_method,omitempty"`
-	PostbackURL          string                 `json:"postback_url,omitempty"`
-	Async                bool                   `json:"async,omitempty"`
-	Installments         int                    `json:"installments,omitempty"`
-	BoletoExpirationDate string                 `json:"boleto_expiration_date,omitempty"`
-	SoftDescriptor       string                 `json:"soft_descriptor,omitempty"`
-	Capture              string                 `json:"capture,omitempty"`
-	BoletoInstructions   string                 `json:"boleto_instructions,omitempty"`
-	SplitRules           []*SplitRule           `json:"split_rules,omitempty"`
-	Customer             *Customer              `json:"customer"`
-	Billing              *Billing               `json:"billing"`
-	Shipping             *Shipping              `json:"shipping"`
-	Items                []*Item                `json:"items,omitempty"`
-	Metadata             map[string]interface{} `json:"metadata,omitempty"`
-	Session              string                 `json:"session,omitempty"`
-	LocalTime            *time.Time             `json:"local_time,omitempty"`
+	Amount               int                    `json:"amount" form:"amount"`
+	CardHash             string                 `json:"card_hash,omitempty" form:"card_hash,omitempty"`
+	CardID               string                 `json:"card_id,omitempty" form:"card_id,omitempty"`
+	CardHolderName       string                 `json:"card_holder_name,omitempty" form:"card_holder_name,omitempty"`
+	CardExpirationDate   MMYY                   `json:"card_expiration_date,omitempty" form:"card_expiration_date,omitempty"`
+	CardNumber           string                 `json:"card_number,omitempty" form:"card_number,omitempty"`
+	CardCVV              string                 `json:"card_cvv,omitempty" form:"card_cvv,omitempty"`
+	PaymentMethod        PaymentMethod          `json:"payment_method,omitempty" form:"payment_method,omitempty"`
+	PostbackURL          string                 `json:"postback_url,omitempty" form:"postback_url,omitempty"`
+	Async                bool                   `json:"async,omitempty" form:"async,omitempty"`
+	Installments         int                    `json:"installments,omitempty" form:"installments,omitempty"`
+	BoletoExpirationDate string                 `json:"boleto_expiration_date,omitempty" form:"boleto_expiration_date,omitempty"`
+	SoftDescriptor       string                 `json:"soft_descriptor,omitempty" form:"soft_descriptor,omitempty"`
+	Capture              string                 `json:"capture,omitempty" form:"capture,omitempty"`
+	BoletoInstructions   string                 `json:"boleto_instructions,omitempty" form:"boleto_instructions,omitempty"`
+	SplitRules           []*SplitRule           `json:"split_rules,omitempty" form:"split_rules,omitempty"`
+	Customer             *Customer              `json:"customer" form:"customer"`
+	Billing              *Billing               `json:"billing" form:"billing"`
+	Shipping             *Shipping              `json:"shipping" form:"shipping"`
+	Items                []*Item                `json:"items,omitempty" form:"items,omitempty"`
+	Metadata             map[string]interface{} `json:"metadata,omitempty" form:"metadata,omitempty"`
+	Session              string                 `json:"session,omitempty" form:"session,omitempty"`
+	LocalTime            *time.Time             `json:"local_time,omitempty" form:"local_time,omitempty"`
 	//
 	// Data returned by pagar-me
 	//
-	Object               string     `json:"object,omitempty"`
-	Status               TrStatus   `json:"status,omitempty"`
-	RefuseReason         string     `json:"refuse_reason,omitempty"`
-	StatusReason         string     `json:"status_reason,omitempty"`
-	AcquirerResponseCode string     `json:"acquirer_response_code,omitempty"`
-	AcquirerName         string     `json:"acquirer_name,omitempty"`
-	AcquirerID           string     `json:"acquirer_id,omitempty"`
-	AuthorizationCode    string     `json:"authorization_code,omitempty"`
-	TID                  int        `json:"tid,omitempty"`
-	NSU                  int        `json:"nsu,omitempty"`
-	DateCreated          *time.Time `json:"date_created,omitempty"`
-	DateUpdated          *time.Time `json:"date_updated,omitempty"`
-	AuthorizedAmount     int        `json:"authorized_amount,omitempty"`
-	PaidAmount           int        `json:"paid_amount,omitempty"`
-	RefundedAmount       int        `json:"refunded_amount,omitempty"`
-	ID                   int64      `json:"id,omitempty"`
-	Cost                 int        `json:"cost,omitempty"`
-	CardFirstDigits      string     `json:"card_first_digits,omitempty"`
-	CardLastDigits       string     `json:"card_last_digits,omitempty"`
-	CardBrand            string     `json:"card_brand,omitempty"`
-	Card                 *Card      `json:"card,omitempty"`
-	CaptureMethod        string     `json:"capture_method,omitempty"`
-	CardPinMode          string     `json:"card_pin_mode,omitempty"`
+	Object               string     `json:"object,omitempty" form:"object,omitempty"`
+	Status               TrStatus   `json:"status,omitempty" form:"status,omitempty"`
+	RefuseReason         string     `json:"refuse_reason,omitempty" form:"refuse_reason,omitempty"`
+	StatusReason         string     `json:"status_reason,omitempty" form:"status_reason,omitempty"`
+	AcquirerResponseCode string     `json:"acquirer_response_code,omitempty" form:"acquirer_response_code,omitempty"`
+	AcquirerName         string     `json:"acquirer_name,omitempty" form:"acquirer_name,omitempty"`
+	AcquirerID           string     `json:"acquirer_id,omitempty" form:"acquirer_id,omitempty"`
+	AuthorizationCode    string     `json:"authorization_code,omitempty" form:"authorization_code,omitempty"`
+	TID                  int        `json:"tid,omitempty" form:"tid,omitempty"`
+	NSU                  int        `json:"nsu,omitempty" form:"nsu,omitempty"`
+	DateCreated          *time.Time `json:"date_created,omitempty" form:"date_created,omitempty"`
+	DateUpdated          *time.Time `json:"date_updated,omitempty" form:"date_updated,omitempty"`
+	AuthorizedAmount     int        `json:"authorized_amount,omitempty" form:"authorized_amount,omitempty"`
+	PaidAmount           int        `json:"paid_amount,omitempty" form:"paid_amount,omitempty"`
+	RefundedAmount       int        `json:"refunded_amount,omitempty" form:"refunded_amount,omitempty"`
+	ID                   int64      `json:"id,omitempty" form:"id,omitempty"`
+	Cost                 int        `json:"cost,omitempty" form:"cost,omitempty"`
+	CardFirstDigits      string     `json:"card_first_digits,omitempty" form:"card_first_digits,omitempty"`
+	CardLastDigits       string     `json:"card_last_digits,omitempty" form:"card_last_digits,omitempty"`
+	CardBrand            string     `json:"card_brand,omitempty" form:"card_brand,omitempty"`
+	Card                 *Card      `json:"card,omitempty" form:"card,omitempty"`
+	CaptureMethod        string     `json:"capture_method,omitempty" form:"capture_method,omitempty"`
+	CardPinMode          string     `json:"card_pin_mode,omitempty" form:"card_pin_mode,omitempty"`
 	// card_magstripe_fallback
-	AntifraudScore float64 `json:"antifraud_score,omitempty"`
-	BoletoURL      string  `json:"boleto_url,omitempty"`
-	BoletoBarcode  string  `json:"boleto_barcode,omitempty"`
-	Referer        string  `json:"referer,omitempty"`
-	IP             string  `json:"ip,omitempty"`
-	SubscriptionID string  `json:"subscription_id,omitempty"`
+	AntifraudScore float64 `json:"antifraud_score,omitempty" form:"antifraud_score,omitempty"`
+	BoletoURL      string  `json:"boleto_url,omitempty" form:"boleto_url,omitempty"`
+	BoletoBarcode  string  `json:"boleto_barcode,omitempty" form:"boleto_barcode,omitempty"`
+	Referer        string  `json:"referer,omitempty" form:"referer,omitempty"`
+	IP             string  `json:"ip,omitempty" form:"ip,omitempty"`
+	SubscriptionID string  `json:"subscription_id,omitempty" form:"subscription_id,omitempty"`
 	// phone
 	// address
-	AntifraudMetadata map[string]interface{} `json:"antifraud_metadata,omitempty"`
+	AntifraudMetadata map[string]interface{} `json:"antifraud_metadata,omitempty" form:"antifraud_metadata,omitempty"`
 	// reference_key
 	// device
 	// local_transaction_id
 	// fraud_covered
 	// order_id
-	RiskLevel string `json:"risk_level,omitempty"`
+	RiskLevel string `json:"risk_level,omitempty" form:"risk_level,omitempty"`
 	// receipt_url
 	// payment
 	// addition
