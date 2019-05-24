ec7c169bc130d543a46238f506c5271730379ee4
gabstv <gabs@gabs.tv>
2019-05-24 13:33:34 -0300

    refund boleto

--- pkg/pagarme/transactions/transactions.go
+++ pkg/pagarme/transactions/transactions.go
@@ -98,8 +98,14 @@ type RefundInput struct {
 	// O estorno parcial obedece as mesmas regras de um estorno total, e usa o parâmetro amount como
 	// referência para o valor a ser estornado. É bom observar que o status da transação vai permanecer
 	// paid até que o valor total da transação tenha sido estornado.
-	Amount         int                     `json:"amount,omitempty"`
-	SplitRules     []*pagarme.SplitRule    `json:"split_rules,omitempty"`
+	Amount      int                    `json:"amount,omitempty"`
+	SplitRules  []*pagarme.SplitRule   `json:"split_rules,omitempty"`
+	Async       bool                   `json:"async,omitempty"`
+	Metadata    map[string]interface{} `json:"metadata,omitempty"`
+	BankAccount *BankAccount           `json:"bank_account,omitempty"`
+}
+
+type BankAccount struct {
 	BankAccountID  string                  `json:"bank_account_id,omitempty"`
 	BankCode       string                  `json:"bank_code,omitempty"`
 	Agencia        string                  `json:"agencia,omitempty"`
@@ -108,9 +114,7 @@ type RefundInput struct {
 	ContaDV        string                  `json:"conta_dv,omitempty"`
 	DocumentNumber string                  `json:"document_number,omitempty"`
 	LegalName      string                  `json:"legal_name,omitempty"`
-	Async          bool                    `json:"async,omitempty"`
 	Type           pagarme.BankAccountType `json:"type,omitempty"`
-	Metadata       map[string]interface{}  `json:"metadata,omitempty"`
 }
 
 // Refund refunds a transaction
--- pkg/pagarme/transactions/transactions_test.go
+++ pkg/pagarme/transactions/transactions_test.go
@@ -3,6 +3,7 @@ package transactions
 import (
 	"io/ioutil"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
@@ -239,6 +240,110 @@ func TestRefundCard(t *testing.T) {
 	}
 	if z1.Status != pagarme.TrRefunded {
 		t.Fatal(z1.Status, "!=", pagarme.TrPaid, z1)
+	}
+}
+
+func TestRefundBoleto(t *testing.T) {
+	adr0 := &pagarme.Address{
+		Zipcode:       "01232000",
+		Street:        "Alameda Barros",
+		StreetNumber:  "400",
+		Complementary: "Apto 1000",
+		State:         "SP",
+		City:          "Sao Paulo",
+		Country:       "br",
+	}
+	tr0 := &pagarme.Transaction{
+		Amount:             4569, // R$ 45.69
+		CardHolderName:     "John Smith",
+		CardExpirationDate: "1250",
+		CardNumber:         "4485038201734841", // test card generated (not real)
+		CardCVV:            "123",              // test env, CVV starting with 6 = authorization error
+		PaymentMethod:      pagarme.PaymentBoleto,
+		Async:              false,
+		Installments:       1,
+		SoftDescriptor:     "teste",
+		Capture:            "true",
+		Customer: &pagarme.Customer{
+			ExternalID: "TESTEJOHN1",
+			Name:       "John Smith",
+			Country:    pagarme.Brazil,
+			Type:       pagarme.CustomerIndividual,
+			Email:      "john.smith@gmail.com",
+			PhoneNumbers: []string{
+				"+5511900000000",
+			},
+			Documents: []*pagarme.Document{
+				&pagarme.Document{
+					Type:   pagarme.DocCPF,
+					Number: "82700962028",
+				},
+			},
+		},
+		Billing: &pagarme.Billing{
+			Name:    "John Smith",
+			Address: adr0,
+		},
+		Shipping: &pagarme.Shipping{
+			Name:         "John Smith",
+			Address:      adr0,
+			DeliveryDate: pagarme.YYYYMMDDFromTime(time.Now().AddDate(0, 0, 2)),
+		},
+		Items: []*pagarme.Item{
+			&pagarme.Item{
+				ID:        "test_1",
+				Title:     "Vitamina C 100mg (100 cap)",
+				UnitPrice: 4568,
+				Tangible:  true,
+				Quantity:  1,
+			},
+		},
+	}
+	t9 := www.JSONReader(tr0)
+	gg, _ := ioutil.ReadAll(t9)
+	t.Log(string(gg))
+	cfg := pagarme.Default(getKeysEnv())
+	//cfg.Trace = true
+	trs := New(cfg)
+	z0, z1, err := trs.Put(tr0)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if z0.Code != pagarme.ResponseCodeOk {
+		t.Fatal(z0.String())
+	}
+	if z1.Status != pagarme.TrProcessing && z1.Status != pagarme.TrWaitingPayment {
+		time.Sleep(time.Second * 2)
+		if _, trstat, _ := trs.Get(strconv.Itoa(int(z1.ID))); trstat == nil || (trstat.Status != pagarme.TrProcessing && trstat.Status != pagarme.TrWaitingPayment) {
+			t.Fatal(trstat.Status, "!=", pagarme.TrProcessing)
+		}
+	}
+
+	if _, _, err := trs.PutDevBillet(strconv.Itoa(int(z1.ID)), pagarme.TrPaid); err != nil {
+		t.Fatal(err)
+	}
+
+	//t.Log(z1)
+	z0, z1, err = trs.Refund(z1.ID, &RefundInput{
+		Amount: 4568,
+		BankAccount: &BankAccount{
+			BankCode:       "237",
+			Agencia:        "1234",
+			Conta:          "123123",
+			ContaDV:        "1",
+			DocumentNumber: "46205518058",
+			LegalName:      "John Smith",
+			Type:           pagarme.BnkAccContaCorrente,
+		},
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if z0.Code != pagarme.ResponseCodeOk {
+		t.Fatal(z0.String())
+	}
+	if z1.Status != pagarme.TrPendingRefund {
+		t.Fatal(z1.Status, "!=", pagarme.TrPendingRefund, z1)
 	}
 }
 
