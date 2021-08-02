86eec525c064ed77a56556b47d6c0cb41cef7645
Mortanius <wilneto17@hotmail.com>
2021-08-02 20:18:41 -0300

    payable additional fields

--- pkg/pagarme/types_payable.go
+++ pkg/pagarme/types_payable.go
@@ -7,22 +7,23 @@ import "time"
 // Objeto contendo os dados de um recebível. O recebível (payable) é gerado automaticamente após uma transação ser paga.
 // Para cada parcela de uma transação é gerado um recebível, que também pode ser dividido por recebedor (no caso de um split ter sido feito).
 type Payable struct {
-	Object           string        `json:"object"`
-	ID               int           `json:"id"`
-	Status           PayableStatus `json:"status"`
-	Amount           int           `json:"amount"`
-	Fee              int           `json:"fee"`
-	AnticipationFee  int           `json:"anticipation_fee"`
-	FraudCoverageFee int           `json:"fraud_coverage_fee"`
-	Installment      int           `json:"installment"`
-	TransactionID    int           `json:"transaction_id"`
-	SplitRuleID      string        `json:"split_rule_id,omitempty"`
-	//TODO: bulk_anticipation_id
-	RecipientID         string        `json:"recipient_id,omitempty"`
-	PaymentDate         time.Time     `json:"payment_date"`
-	OriginalPaymentDate time.Time     `json:"original_payment_date,omitempty"`
-	Type                PayableType   `json:"type"`
-	PaymentMethod       PaymentMethod `json:"payment_method"`
-	AccrualDate         time.Time     `json:"accrual_date,omitempty"`
-	DateCreated         time.Time     `json:"date_created"`
+	Object                   string        `json:"object"`
+	ID                       int           `json:"id"`
+	Status                   PayableStatus `json:"status"`
+	Amount                   int           `json:"amount"`
+	Fee                      int           `json:"fee"`
+	AnticipationFee          int           `json:"anticipation_fee"`
+	FraudCoverageFee         int           `json:"fraud_coverage_fee"`
+	Installment              int           `json:"installment"`
+	TransactionID            int           `json:"transaction_id"`
+	SplitRuleID              string        `json:"split_rule_id,omitempty"`
+	BulkAnticipationID       string        `json:"bulk_anticipation_id,omitempty"`
+	RecipientID              string        `json:"recipient_id,omitempty"`
+	PaymentDate              time.Time     `json:"payment_date"`
+	OriginalPaymentDate      time.Time     `json:"original_payment_date,omitempty"`
+	Type                     PayableType   `json:"type"`
+	PaymentMethod            PaymentMethod `json:"payment_method"`
+	AccrualDate              time.Time     `json:"accrual_date,omitempty"`
+	DateCreated              time.Time     `json:"date_created"`
+	LiquidationArrangementID string        `json:"liquidation_arrangement_id,omitempty"`
 }
