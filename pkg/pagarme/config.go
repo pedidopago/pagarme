2d21eb9405b4e02ef419b2ae00b01c2ca6080350
Mortanius <wilneto17@hotmail.com>
2021-08-11 12:32:39 -0300

    rate limit error handling

--- pkg/pagarme/config.go
+++ pkg/pagarme/config.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"net/http"
+	"strconv"
 	"time"
 
 	"go.uber.org/zap"
@@ -18,6 +20,15 @@ type Config struct {
 	Client    *http.Client
 	Trace     bool
 	Logger    *zap.Logger
+
+	HandleRateLimit bool
+	TimeoutRetry time.Duration
+	ExpBackOffParams *ExponentialBackoffParams
+}
+
+type ExponentialBackoffParams struct {
+	MaxRetries int
+	IntervalBase time.Duration
 }
 
 // Default configuration uses:
@@ -78,5 +89,57 @@ func (c *Config) Do(method, urlpart string, body io.Reader) (*http.Response, err
 	if c.Client == nil {
 		return http.DefaultClient.Do(req)
 	}
-	return c.Client.Do(req)
+	resp, err := c.Client.Do(req)
+
+	if err == nil && resp != nil && resp.StatusCode == http.StatusTooManyRequests && c.HandleRateLimit {
+		return c.rateLimitReachedHandler(req, resp, err)
+	}
+
+	return resp, err
 }
+
+func (c *Config) rateLimitReachedHandler(req *http.Request, inresp *http.Response, inerr error) (*http.Response, error) {
+	c.Logger.Debug("rateLimitReachedHandler")
+	retryAfterSec, _ := strconv.Atoi(inresp.Header.Get("Retry-After"))
+	retryAfter := time.Duration(retryAfterSec) * time.Second
+	if c.TimeoutRetry != 0 {
+		if retryAfter > c.TimeoutRetry {
+			c.Logger.Debug("rateLimitReachedHandler: timeout retry non sufficient (1)")
+			return inresp, inerr
+		}
+	}
+
+	retryStartedAt := time.Now()
+
+	time.Sleep(retryAfter)
+	if c.ExpBackOffParams == nil {
+		return c.Client.Do(req)
+	}
+
+	var resp = inresp
+	var rerr = inerr
+	var intervalBase = float64(c.ExpBackOffParams.IntervalBase.Milliseconds())
+	for attempt := 1; attempt <= c.ExpBackOffParams.MaxRetries; attempt++ {
+		var waitFor = time.Millisecond * time.Duration(intervalBase * math.Pow(2, float64(attempt - 1)))
+		if c.TimeoutRetry != 0 {
+			if time.Now().Sub(retryStartedAt) + waitFor > c.TimeoutRetry {
+				c.Logger.Debug("rateLimitReachedHandler: timeout retry non sufficient (2)")
+				return resp, inerr
+			}
+		}
+		c.Logger.Debug(fmt.Sprint("attempt ", attempt, "sleeping for ", waitFor))
+		time.Sleep(waitFor)
+		resp, rerr = c.Client.Do(req)
+		if rerr != nil {
+			return nil, rerr
+		}
+		if resp != nil && resp.StatusCode != http.StatusTooManyRequests {
+			break
+		}
+	}
+
+	return resp, rerr
+}
+
+
+
